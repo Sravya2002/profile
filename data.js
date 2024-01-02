ac4a400c8fa4cd9f9cd8a41b519cc3dc724f0315
cvu sravya <chadalapakasravya@gmail.com>
2024-01-02 17:11:33 +0530

    the files are added

---
+++ data.js
@@ -0,0 +1,17 @@
+const servicesData=[
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['chjxksznxcsz']
+    },
+    {
+        title: "Backend Development",
+        category: "backend",
+        description: ['chjxksznxcsz']
+    },
+    {
+        title: "Fullstack Development",
+        category: "fullstack",
+        description: ['chjxksznxcsz']
+    },
+]
